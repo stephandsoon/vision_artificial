f9cd5a2345deff47797f4ab135a11e128d869cb2
Stephan <stephan.hoetger@kabelmail.de>
2023-11-20 14:07:50 -0500

    20.11.2023, 02:05 pm: Se baja la calidad de la imagen para la primera etapa de la detección (detección del carro) Sin embargo, para sacar la imagen recortada del vehiculo y para sacar la imagen de la placa se utiliza la imagen original. También se puede bajar la framerate mediante parametro vid_stride

--- aplication_detect_cars_and_lp_video.py
+++ aplication_detect_cars_and_lp_video.py
@@ -10,9 +10,18 @@ lp_model = YOLO('models/plate_model.pt')
 video_path = "BigFiles/minas_parqueadero_SH.mp4"
 # Create a VideoCapture object
 cap = cv2.VideoCapture(video_path)
+
 # Check if the video capture object is successfully opened
 if not cap.isOpened():
     print("Error: Unable to open the video file.")
+
+# Lower the frame´s resolution by the factor of resolution_factor
+resolution_factor = 1/10
+# Get the original video´s size and resize it to not damage the ratio aspect
+original_width_vid = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+original_height_vid = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+changed_width_vid = int(round(original_width_vid*resolution_factor,0))
+changed_height_vid = int(round(original_height_vid*resolution_factor,0))
 
 
 while True:
@@ -23,7 +32,9 @@ while True:
         print("End of video.")
         break
     
-    results = model(frame, imgsz=320, stream=True, verbose=False)
+    # Lower the frames quality for the first prediction (vehicle prediction)
+    frame_redimensionized = cv2.resize(frame, (changed_width_vid, changed_height_vid))
+    results = model(frame_redimensionized, imgsz=320, stream=True, verbose=False, vid_stride=10) #vid_stride defines framerate: every n frames 1 frame is being processed by the ANN YOLO
     for result in results:
          for box in result.boxes.cpu().numpy():
             # print(box)
@@ -31,15 +42,23 @@ while True:
             conf = box.conf[0]
 
             vehicle_found = cls in [2,3,5,7] and conf > 0.65 #  2: 'car',  3: 'motorcycle',  5: 'bus', 7: 'truck',
-            labels = {2: 'car',  3: 'motorcycle',  5: 'bus', 7: 'truck'}
+            labels = {2:'car',3:'motorcycle',5:'bus',7:'truck'}
 
             if vehicle_found:
                 # Define box coordinates
-                r = box.xyxy[0].astype(int) # -> r[0]=x1; r[1]=y1; r[2]=x2; r[3]=y2;
+                r = (box.xyxy[0]/resolution_factor).astype(int) # -> r[0]=x1; r[1]=y1; r[2]=x2; r[3]=y2;
+                if r[0] < 0:
+                    r[0] = 0
+                if r[1] < 0:
+                    r[1] = 0
+                if r[2] > original_width_vid:
+                    r[2] = original_width_vid
+                if r[3] > original_height_vid:
+                    r[3] = original_height_vid
                 w = r[2] - r[0]
                 h = r[3] - r[1]
                 # Draw a rectangle around that found object if it´s fulfilles the requirements (vehicle with certainty >0.65)
-                cv2.rectangle(frame, (r[0],r[1]), (r[2],r[3]), (255, 255, 255), 1) #r[:2], r[2:], (255, 255, 255), 2)
+                cv2.rectangle(frame, (r[0],r[1]), (r[2],r[3]), (255, 255, 255), 2) #r[:2], r[2:], (255, 255, 255), 2)
                 # Extract the region inside the rectangle
                 vehicle_frame = frame[r[1]:r[3], r[0]:r[2]]
                 # Save the extracted region to a PNG file with the current timestamp
@@ -49,7 +68,7 @@ while True:
                 cv2.imwrite(filename, vehicle_frame)
 
                 #From here on: pass the vehicle_frame to that placa model.
-                lp_results = lp_model(vehicle_frame, imgsz=320, stream=True, verbose=False)
+                lp_results = lp_model(vehicle_frame, imgsz=320, stream=True, verbose=False, vid_stride=1)
                 for lp_result in lp_results:
                     for lp_box in lp_result.boxes.cpu().numpy():
                         lp_cls = int(lp_box.cls[0])
@@ -93,6 +112,6 @@ cap.release()
 cv2.destroyAllWindows()
 
 # to-do:
-# 1. Modelo para leer caracteres teniendo en cuenta la clasificación #1
-# 2. Crear mécanismo encuentre la imagen / la detección de la placa mejor
-# 3. Utilizar metodos de la clase en cuanto al preprocesamiento de las placas. 
+# 1. Utilizar metodos de la clase en cuanto al preprocesamiento de las placas. 
+# 2. Modelo para leer caracteres teniendo en cuenta la clasificación del tipo del vehiculo (moto o carro)
+# 3. Crear mécanismo que encuentre la imagen / la detección de la placa mejor
