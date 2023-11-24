6c59774a696e69de8fb1795b4ee75f34b429a425
Jorge Torres <Jor95t@gmail.com>
2023-11-24 00:10:13 -0500

    Testing sizes and  char model

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+import Models
+Models.plate = './Models/plate_model.pt'
+Models.charbox = './Models/boxes_N50.pt'
+Models.number = './Models/best_Numbers_N100-224.pt'
+Models.bikeletter = './Models/best_Letter_Bike__N100-224.pt'
+Models.letter = './Models/best_Letters_N100-224.pt'
+Models.yolov8n = './Models/yolov8n.pt'
