b0766ca52b345869e962893631e441deaaf7a092
Jorge Torres <Jor95t@gmail.com>
2023-11-24 07:24:18 -0500

    New plate model

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
-import Models
-Models.plate = './Models/plate_model.pt'
-Models.charbox = './Models/boxes_N50.pt'
-Models.number = './Models/best_Numbers_N100-224.pt'
-Models.bikeletter = './Models/best_Letter_Bike__N100-224.pt'
-Models.letter = './Models/best_Letters_N100-224.pt'
-Models.yolov8n = './Models/yolov8n.pt'
+import models
+models.plate = './models/plate_model.pt'
+models.plate2 = './models/best_plate_N23-640.pt'
+models.plate3 = './models/last_plate_N23-640.pt'
+models.charbox = './models/boxes_N50.pt'
+models.number = './models/best_Numbers_N100-224.pt'
+models.bikeletter = './models/best_Letter_Bike__N100-224.pt'
+models.letter = './models/best_Letters_N100-224.pt'
+models.yolov8n = './models/yolov8n.pt'
